dce1eb3364b520183c7f39cfb23ce22cd88ee2db
Arbaj Ansari <arbaj897ansari@gmail.com>
2024-12-15 20:25:22 +0530

    React Navigation implemented

--- App.jsx
+++ App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import {Text} from 'react-native'
-const App = () => {
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
-    <Text>App</Text>
-  )
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
 
-export default App
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
