07123dc0842cd53dee6177aa32c9e3f8daa3278a
Umar Shaikh <umarsk9921@gmail.com>
2024-12-18 20:47:32 +0530

    ZC-27: Splash screen created

--- screens/SplashScreen.jsx
+++ screens/SplashScreen.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { Text } from 'react-native'
-import Layout from '../components/layout/Layout'
+import React from 'react';
+import Layout from '../components/layout/Layout';
+import Typography from '../components/typography/Typography';
+import BrandSvg from '../assets/icons/BrandSvg.jsx';
+import MakeInIndia from '../assets/icons/MakeInIndiaSvg.jsx';
 
 const SplashScreen = () => {
   return (
-    <Layout style={{
-        justifyContent : 'center',
-        alignItems : 'center'
-    }} >
-    <Text>SplashScreen</Text>
+    <Layout
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <BrandSvg />
+      <Typography variant="base" color="primary">
+        Zerox Cloud
+      </Typography>
+      <Typography variant="caption" color="primary">
+        Ab Xerox ban gaya easy!
+      </Typography>
+      <MakeInIndia style={{marginTop: 300}} />
     </Layout>
-  )
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
