44cb2da95242d85800311e208375e8192ecd1475
Arbaj Ansari <arbaj897ansari@gmail.com>
2024-12-18 17:04:52 +0530

    made changes in Button and TextField

--- components/form/TextField/index.jsx
+++ components/form/TextField/index.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { Text, TextInput, View, StyleSheet } from 'react-native';
+import React, { forwardRef } from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
 import colors from '../../../utils/colors';
 
-const TextField = ({ value, label, onChange, startIcon: StartIcon, endIcon: EndIcon, style }) => {
-  return (
-    <View style={[styles.container, style]}>
-      {StartIcon && (
-        <View style={styles.startIconContainer}>
-          {/* <Text style={styles.iconStyle}>S</Text> */}
-          <StartIcon {...styles.iconStyle} />
-        </View>
-      )}
-      <TextInput
-        style={styles.input}
-        placeholder={label}
-        value={value}
-        onChangeText={onChange}
-        placeholderTextColor={colors.placeholder}
-      />
-      {EndIcon && (
-        <View style={styles.endIconContainer}>
-          <EndIcon {...styles.iconStyle} />
-
-        </View>
-      )}
-    </View>
-  );
-};
+const TextField = forwardRef(
+  ({ value, label, onChange, startIcon: StartIcon, endIcon: EndIcon, style, placeholderTextColor, autoFocus }, ref = null) => {
+    return (
+      <View style={[styles.container, style]}>
+        {StartIcon && (
+          <View style={styles.startIconContainer}>
+            <StartIcon {...styles.iconStyle} />
+          </View>
+        )}
+        <TextInput
+          ref={ref}
+          style={styles.input}
+          placeholder={label}
+          value={value}
+          onChangeText={onChange}
+          placeholderTextColor={placeholderTextColor || colors.placeholder}
+          autoFocus={autoFocus}
+        />
+        {EndIcon && (
+          <View style={styles.endIconContainer}>
+            <EndIcon {...styles.iconStyle} />
+          </View>
+        )}
+      </View>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -40,10 +42,14 @@ const styles = StyleSheet.create({
   startIconContainer: {
     padding: 8,
     paddingRight: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   endIconContainer: {
     padding: 8,
     paddingLeft: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   iconStyle: {
     color: colors.grayDark,
