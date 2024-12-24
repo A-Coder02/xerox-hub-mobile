01c6488208b66dde02f5baedfd49ea643f941068
Arbaj Ansari <arbaj897ansari@gmail.com>
2024-12-24 19:04:19 +0530

    made some final changes

--- screens/auth/CreateAccount.jsx
+++ screens/auth/CreateAccount.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import Typography from '../../components/typography/Typography';
 import AppBar from '../../components/layout/AppBar';
 import TextField from '../../components/form/TextField';
@@ -7,7 +7,7 @@ import MailSvg from '../../assets/icons/MailSvg';
 import UserSvg from '../../assets/icons/UserSvg';
 import Button from '../../components/form/Button';
 
-const CreateAccount = ({onNavigate}) => {
+const CreateAccount = ({ onPress }) => {
   // State variables for the input fields
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
@@ -20,7 +20,7 @@ const CreateAccount = ({onNavigate}) => {
     console.log('Mobile:', mobile);
   };
 
-  const {container, formContainer, textFieldContainer} = styles;
+  const { container, formContainer, textFieldContainer } = styles;
   return (
     <View style={container}>
       <View style={formContainer}>
@@ -28,12 +28,12 @@ const CreateAccount = ({onNavigate}) => {
           <AppBar
             title="Create Account"
             isBottomSheet
-            onNavigate={onNavigate}
+            onPress={onPress}
             textColor={colors.primary}
             textVariant="h2"
           />
           <Typography
-            style={{paddingBottom: 8, marginBottom: 32}}
+            style={{ paddingBottom: 8, marginBottom: 32 }}
             variant="caption"
             color="grayDark">
             To use services, please create account
@@ -72,8 +72,8 @@ const CreateAccount = ({onNavigate}) => {
 };
 
 const styles = StyleSheet.create({
-  container: {gap: 30},
-  formContainer: {height: 350},
+  container: { gap: 30 },
+  formContainer: { height: 350 },
   textFieldContainer: {
     gap: 32,
   },
