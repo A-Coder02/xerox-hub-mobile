7835be2c9bd53e00ec6d216be9feb71fec43c672
Arbaj Ansari <arbaj897ansari@gmail.com>
2024-12-24 20:23:34 +0530

    made changes in Appbar

--- components/layout/AppBar/index.jsx
+++ components/layout/AppBar/index.jsx
@@ -4,42 +4,42 @@ import LeftArrowSvg from '../../../assets/icons/LeftArrowSvg'
 import colors from '../../../utils/colors'
 import Typography from '../../typography/Typography'
 import {
-    useNavigation,
+  useNavigation,
 } from '@react-navigation/native';
 import { TouchableHighlight } from 'react-native-gesture-handler'
-const AppBar = ({ title, isBottomSheet = false, onPress = () => { } }) => {
-    const navigation = useNavigation()
-    return (
-        <View style={[styles.header, {
-            flexDirection: isBottomSheet ? 'row-reverse' : 'row'
+const AppBar = ({ title, isBottomSheet = false, onPress = () => { }, style = {}, titleStyle = {} }) => {
+  const navigation = useNavigation()
+  return (
+    <View style={[styles.header, {
+      flexDirection: isBottomSheet ? 'row-reverse' : 'row'
+    }, style]}>
+      <TouchableHighlight
+        onPress={() => isBottomSheet ? onPress() : navigation.goBack()}
+        underlayColor={colors.underlayColor}
+        style={[styles.backButton, {
+          transform: [{ rotate: isBottomSheet ? '-90deg' : "0deg" }]
         }]}>
-            <TouchableHighlight
-                onPress={() => isBottomSheet ? onPress() : navigation.goBack()}
-                underlayColor={colors.underlayColor}
-                style={[styles.backButton, {
-                    transform: [{ rotate: isBottomSheet ? '-90deg' : "0deg" }]
-                }]}>
-                <LeftArrowSvg />
-            </TouchableHighlight>
-            {title && <Typography variant='h1' fontSize={18} fontWeight={600} style={{
-                flex: 1
-            }} >
-                {title}
-            </Typography>}
-        </View>
-    )
+        <LeftArrowSvg />
+      </TouchableHighlight>
+      {title && <Typography variant='h1' fontSize={18} fontWeight={600} style={[{
+        flex: 1
+      }, titleStyle]} >
+        {title}
+      </Typography>}
+    </View>
+  )
 }
 
 export default AppBar;
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 16,
-    },
-    backButton: {
-        backgroundColor: colors.grayDark,
-        borderRadius: 100,
-    },
-},)
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  backButton: {
+    backgroundColor: colors.grayDark,
+    borderRadius: 100,
+  },
+});
