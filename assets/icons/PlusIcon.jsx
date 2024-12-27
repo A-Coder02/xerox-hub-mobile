7eccea84a6535c4af905bb327d79820a8ecd822d
naiem786 <naiemhshaikh786@gmail.com>
2024-12-27 21:20:32 +0530

    File upload is working , plus icon file cannot be change , made some changes in colors folder

--- assets/icons/PlusIcon.jsx
+++ assets/icons/PlusIcon.jsx
@@ -1,23 +1,23 @@
 import * as React from "react";
-import Svg, { Path } from "react-native-svg";
-
-const PlusSvg = (props) => (
+import Svg, { G, Rect, Path } from "react-native-svg";
+import colors from "../../utils/colors";
+const PlusIcon = ({ size = 100, ...props }) => (
   <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={props.width}
-    height={props.height}
+    width={size}
+    height={size}
+    viewBox="0 0 64 64"
     fill="none"
-    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <Path
-      stroke={props.color}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={1.5}
-      d="M16 8v16M8 16h16"
-    />
+    <G opacity={0.64}>
+      <Rect width={64} height={64} rx={32} fill={colors.plusIcon} />
+      <Path
+        d="M30.6667 33.3334H22.6667V30.6667H30.6667V22.6667H33.3334V30.6667H41.3334V33.3334H33.3334V41.3334H30.6667V33.3334Z"
+        fill="white"
+      />
+    </G>
   </Svg>
 );
 
-export default PlusSvg;
+export default PlusIcon;
--- assets/icons/UploadSvgIcon.jsx
+++ assets/icons/UploadSvgIcon.jsx
@@ -1,20 +1,30 @@
-// UploadSvgIcon.js
-import React from 'react';
-import Svg, { Path } from 'react-native-svg';
+import * as React from "react";
+import Svg, { Rect, Path } from "react-native-svg";
 
 const UploadSvgIcon = (props) => (
   <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={props.width || 24}  // Default width
-    height={props.height || 24} // Default height
-    viewBox="0 0 24 24"
+    width={24}
+    height={25}
+    viewBox="0 0 24 25"
     fill="none"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <Path
-      d="M12 2L12 14M12 14L9 11M12 14L15 11M18 21L6 21"
-      stroke={props.strokeColor || '#000'} // Default stroke color
-      strokeWidth={2}
+      d="M8.0625 8.1875L12 4.25L15.9375 8.1875"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M12 14.75V4.25"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M20.25 14.75V20C20.25 20.1989 20.171 20.3897 20.0303 20.5303C19.8897 20.671 19.6989 20.75 19.5 20.75H4.5C4.30109 20.75 4.11032 20.671 3.96967 20.5303C3.82902 20.3897 3.75 20.1989 3.75 20V14.75"
+      stroke="white"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
